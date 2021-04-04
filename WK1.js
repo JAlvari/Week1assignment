edfcd21b2ba4c6c21ca6a2d455f9d33dd1f6e71c
unknown <thecrazymonkey7@gmail.com>
2021-04-03 20:26:58 -0600

    first commit

---
+++ WK1.js
@@ -0,0 +1,14 @@
+var itemPrice = 30;
+
+var moneyInWallet = 50;
+
+var numFrnds = 5;
+
+var age = 30;
+
+var fistName = 'Josh';
+var lastName = 'Alvari';
+var middleInitial = 'E';
+
+console.log(moneyInWallet - itemPrice);
+console.log(fistName + middleInitial + lastName);
---
+++ index.js
@@ -0,0 +1,15 @@
+var itemPrice = 25;
+
+var moneyInWallet = 50;
+
+var numFrnds = 43;
+
+var age = 30;
+
+var fistName = 'Josh';
+var lastName = 'Alvari';
+var middleInitial = 'E';
+
+console.log(moneyInWallet - itemPrice);
+console.log(fistName + " " + middleInitial + " " + lastName);
+console.log(numFrnds / age)
